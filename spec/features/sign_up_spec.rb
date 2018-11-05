994f2a7a385f9517c08de5d2373ee4226c2b8a5e
Becka <rebdok@hotmail.com>
2018-11-05 16:34:42 +0000

    sign up and sign out only appear when appropriate

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -14,4 +14,19 @@ feature do
     expect(page).to have_content "Welcome to MakersBnB: fakeemail@mail.com"
   end
 
+  it "doesn't see user sign out button if not signed in" do
+    visit '/'
+    expect(page).to have_button 'sign up'
+    expect(page).not_to have_button 'sign out'
+  end
+
+  it "doesn't see sign in buttonif already signed in" do
+    visit'/sign_up'
+    fill_in 'email', with: "fakeemail@mail.com"
+    fill_in 'password', with: "fakepassword"
+    click_button 'submit'
+    expect(page).to have_button 'sign out'
+    expect(page).not_to have_button 'sign up'
+
+  end
 end
