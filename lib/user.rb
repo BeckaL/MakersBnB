851d78888ccab434e3ae52ecb4dbc9081c42710c
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-06 14:17:50 +0000

    Deleted database connection

--- lib/user.rb
+++ lib/user.rb
@@ -4,7 +4,6 @@ require './lib/database_connection'
 class User
 
   def self.add(email:, password:)
-    connect_to_database
     raise "email taken!" unless DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}'").first.nil?
     DatabaseConnection.query("INSERT INTO users (email, password) VALUES('#{email}',
     '#{password}') RETURNING user_id, email, password")
@@ -12,16 +11,8 @@ class User
   end
 
   def self.all
-    connect_to_database
     data = DatabaseConnection.query("SELECT email FROM users")
     data.map { |user| user['email'] }
-  end
-
-  private
-
-  def self.connect_to_database
-    return DatabaseConnection.setup(dbname: "makers_bnb_test") if ENV['RACK_ENV'] == "test"
-    return DatabaseConnection.setup(dbname: 'makers_bnb')
   end
 
 end
