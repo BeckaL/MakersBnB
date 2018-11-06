53114003de471d40a6d5a9999b876551e5e09ab7
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-06 10:25:14 +0000

    Added database connection class to setup the connection

---
+++ spec/database_spec.rb
@@ -0,0 +1,10 @@
+require 'database_connection'
+
+describe DatabaseConnection do
+  describe '.setup' do
+    it 'sets up a connection to a database through PG' do
+      expect(PG).to receive(:connect).with(dbname: 'makers_bnb_test')
+      DatabaseConnection.setup('makers_bnb_test')
+    end
+  end
+end
