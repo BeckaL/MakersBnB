af31e417de7f7a4939847786b7eed8548ac27ff0
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-06 11:12:15 +0000

    Extracted database methods from user class

--- user.rb
+++ user.rb
@@ -3,22 +3,22 @@ require 'pg'
 class User
 
   def self.add(email:, password:)
-    connection = connect_to_database
-    connection.exec("INSERT INTO users (email, password) VALUES('#{email}',
+    DatabaseConnection.query("INSERT INTO users (email, password) VALUES('#{email}',
     '#{password}') RETURNING user_id, email, password")
     return email
   end
 
   def self.all
-    connection = connect_to_database
-    data = connection.exec("SELECT email FROM users")
+    data = DatabaseConnection.query("SELECT email FROM users")
     data.map { |user| user['email'] }
   end
 
-  def self.connect_to_database
-    return PG.connect :dbname => "makers_bnb_test" if ENV['RACK_ENV'] == "test"
 
-    return PG.connect :dbname => "makers_bnb"
+  private
+
+  def self.connect_to_database
+    return DatabaseConnection.setup(dbname: "makers_bnb_test") if ENV['RACK_ENV'] == "test"
+    return DatabaseConnection.setup(dbname: 'makers_bnb')
   end
 
 end
