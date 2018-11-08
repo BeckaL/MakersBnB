8d197de452b99d96b73f4d36ef981569df84872b
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-08 11:16:42 +0000

    Feature tests for conditional buttons

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -1,16 +1,28 @@
 feature "Booking a space" do
 
-  before do
-    sign_up_generic_user
-    create_new_generic_listing
+  context 'user not signed in' do
+
+    it 'should not have request-to-book-button if the user is not logged in' do
+      expect(page).to have_no_button('request to book')
+    end
+
   end
 
-  it 'takes a booking for an available date' do
-    click_link "Beckas mansion"
-    click_link "request to book"
-    select "2019-01-01", :from => "available dates"
-    click_button 'submit'
-    expect(page).to have_content 'booking request sent'
+  context 'user signed in' do
+
+    before do
+      sign_up_generic_user
+      create_new_generic_listing
+    end
+
+    it 'takes a booking for an available date' do
+      click_link "Beckas mansion"
+      click_link "request to book"
+      select "2019-01-01", :from => "available dates"
+      click_button 'submit'
+      expect(page).to have_content 'booking request sent'
+    end
+
   end
 
 
--- spec/features/listing_spec.rb
+++ spec/features/listing_spec.rb
@@ -1,27 +1,39 @@
 feature do
 
-  before do
-    sign_up_generic_user
+  context 'user is not signed in' do
+
+    it 'should not have create-new-listing-button if the user is not logged in' do
+      expect(page).to have_no_button('create new listing')
+    end
+
   end
 
-  it 'has a new listing page' do
-    click_button 'create new listing'
-    expect(page).to have_field "name"
-    expect(page).to have_field "description"
-    expect(page).to have_field "price"
-  end
+  context 'user is signed in' do
 
-  it 'creates a new listing' do
-    create_new_generic_listing
-    expect(page).to have_content "Beckas mansion"
-  end
+    before do
+      sign_up_generic_user
+    end
 
-  it 'creates two listings' do
-    create_new_generic_listing
-    create_new_listing("An MP's second home", "Happily bought on taxpayer dollar", '5000', '2019-01-01')
+    it 'has a new listing page' do
+      click_button 'create new listing'
+      expect(page).to have_field "name"
+      expect(page).to have_field "description"
+      expect(page).to have_field "price"
+    end
 
-    expect(page).to have_content "Beckas mansion"
-    expect(page).to have_content "An MP's second home"
+    it 'creates a new listing' do
+      create_new_generic_listing
+      expect(page).to have_content "Beckas mansion"
+    end
+
+    it 'creates two listings' do
+      create_new_generic_listing
+      create_new_listing("An MP's second home", "Happily bought on taxpayer dollar", '5000', '2019-01-01')
+
+      expect(page).to have_content "Beckas mansion"
+      expect(page).to have_content "An MP's second home"
+    end
+
   end
 
 end
