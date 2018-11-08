197e324d75605519122df14c7a987ae6106ffe57
Becka <rebdok@hotmail.com>
2018-11-08 13:03:22 +0000

    booking method .all updated

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,26 +1,35 @@
 
 class Booking
+  attr_reader :booking_id, :guest_id, :host_id, :listing_id, :date
 
- def self.create(guest_id:, host_id:, listing_id:, date:)
-   DatabaseConnection.setup
+  def initialize(booking_id:, guest_id:, host_id:, listing_id:, date:)
+    @booking_id = booking_id
+    @guest_id = guest_id
+    @host_id = host_id
+    @listing_id = listing_id
+    @date = date
+  end
 
-   result = DatabaseConnection.query("INSERT INTO bookings(guest_id, host_id, listing_id, date)
-   VALUES ('#{guest_id}','#{host_id}','#{listing_id}', '#{date}')
-   RETURNING booking_id, guest_id, host_id, listing_id, date")
- end
+  def self.create(guest_id:, host_id:, listing_id:, date:)
+    DatabaseConnection.setup
 
- def self.all
-   DatabaseConnection.setup
-   result = DatabaseConnection.query("SELECT * FROM bookings")
-   result.map do |request|
-     name = CGI.unescape(request['name'])
-     description = CGI.unescape(request['description'])
-     Booking.new(
-       booking_id: listing['listing_id'].to_i,
-       guest_id: listing['user_id'].to_i,
-       host_id: name,
-       listing_id: [',
-       dates: reqest['dates']
- end
+    result = DatabaseConnection.query("INSERT INTO bookings(guest_id, host_id, listing_id, date)
+    VALUES ('#{guest_id}','#{host_id}','#{listing_id}', '#{date}')
+    RETURNING booking_id, guest_id, host_id, listing_id, date")
+  end
+
+  def self.all
+    DatabaseConnection.setup
+    result = DatabaseConnection.query("SELECT * FROM bookings")
+    result.map do |request|
+      Booking.new(
+        booking_id: request['booking_id'].to_i,
+        guest_id: request['guest_id'].to_i,
+        host_id: request['host_id'].to_i,
+        listing_id: request['listing_id'].to_i,
+        date: request['date']
+      )
+    end
+  end
 
 end
